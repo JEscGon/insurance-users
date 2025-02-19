045443462eff1b67d164d0724445631bb33d8b74
Jesús Escolar González <jesuse.g@outlook.es>
2025-02-19 16:52:06 +0100

    Initial Commit.

---
+++ src/main/java/com/dev/insurance_users/InsuranceUsersApplication.java
@@ -0,0 +1,13 @@
+package com.dev.insurance_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceUsersApplication.class, args);
+	}
+
+}
