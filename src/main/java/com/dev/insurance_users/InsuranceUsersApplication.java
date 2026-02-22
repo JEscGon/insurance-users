c166308a307b42c36cf67b2411bf123726726657
ANONIMO\Jesus <jesusescolargonzalez@gmail.com>
2026-02-22 21:47:54 +0100

    fix: configserver URL + restore Application.java

--- src/main/java/com/dev/insurance_users/InsuranceUsersApplication.java
+++ src/main/java/com/dev/insurance_users/InsuranceUsersApplication.java
@@ -2,14 +2,11 @@ package com.dev.insurance_users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@ComponentScan(basePackages = {"com.dev.insurance_users"})
 public class InsuranceUsersApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(InsuranceUsersApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceUsersApplication.class, args);
+    }
 }
